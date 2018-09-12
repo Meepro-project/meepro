13aaeb7349a83de09c8d088ec0950775845fa0db
cookie-s <cookie-s@users.noreply.github.com>
2018-09-13 07:05:23 +0900

    refactoring

--- components/FacebookLoginButton.vue
+++ components/FacebookLoginButton.vue
@@ -2,27 +2,19 @@
   <button class="btn btn-social btn-facebook" @click="facebookLogin">
     <fa style="margin: auto;" :icon="['fab','facebook-square']" />Sign in with Facebook
   </button>
-  <!-- img class="facebook-login-button" src="/facebook.png" @click="facebookLogin" / -->
 </template>
 
 <script>
 import firebase from '~/utils/firebase.js';
-import 'firebase/auth';
-
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import {auth} from '~/utils/firebase/auth.js';
 
 export default {
   props: ['to'],
-  computed: {
-    faFacebook() {
-      return faFacebook;
-    },
-  },
   methods: {
     facebookLogin() {
       const provider = new firebase.auth.FacebookAuthProvider();
 
-      firebase.auth().signInWithPopup(provider).then( async _ => {
+      auth.signInWithPopup(provider).then( async _ => {
         await this.$store.dispatch('CHANGE_USER');
         if(this.to) {
           this.$router.push(this.to);
