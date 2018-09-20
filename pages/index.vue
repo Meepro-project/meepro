aba50b7c586bc4c46bd55f350188855b7f96a759
Yuka Takahashi <yukatkh@gmail.com>
2018-09-20 22:01:34 +0200

    Add fork me on github

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,12 @@
           to="/profile" />
       </div>
     </section>
+    <a href="https://github.com/Meepro-project/meepro">
+      <img
+        style="position: absolute; top: 0; right: 0; border: 0;"
+        src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+        alt="Fork me on GitHub"></a>
+
   </div>
 </template>
 
